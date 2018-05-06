9057ce729eb3ac59013dda53b89d69760c707b0b
Ivan Bozhilov <ivanbojilov@gmail.com>
2018-05-06 22:04:45 +0300

    Implementation of Msgpack.Paker protocol for Float and Integer

--- lib/msgpack/pack.ex
+++ lib/msgpack/pack.ex
@@ -19,3 +19,22 @@ defimpl Msgpack.Paker, for: Atom do
     atom |> Atom.to_string() |> Msgpack.Paker.pack()
   end
 end
+
+defimpl Msgpack.Paker, for: Float do
+  def pack(num) do
+    <<0xCB, num::float>>
+  end
+end
+
+defimpl Msgpack.Paker, for: Integer do
+  def pack(0) do 
+    <<0xd0, 0::signed-integer>>
+  end
+  
+  def pack(int) do
+    header = int |> :math.log2() |> :math.ceil() 
+    |> Kernel.+(1) |> Kernel./(8) |> :math.ceil() 
+    |> :math.log2() |> :math.ceil() |> Kernel.trunc() |> Kernel.+(0xd0)
+    <<header, int::signed-integer>>
+  end
+end
