37aac641eb5811986559480c4b19daa1061220b6
Ivan Bozhilov <ivanbojilov@gmail.com>
2018-05-06 23:00:54 +0300

    Implementing string parser

--- lib/json/parser/string.ex
+++ lib/json/parser/string.ex
@@ -1,5 +1,30 @@
 defmodule JSON.Parser.String do
-	def parse(json) do
-		"string " <> json
-	end 
+	def parse(<<char::utf8, rest::binary>>) when char == 34 do
+		case parse_string([], rest) do
+			{:ok, result} -> result
+			{:error, reason} -> {:error, reason}
+		end
+	end
+
+	def parse(_) do
+		{:error, "Unexpected data"}
+	end
+
+	defp parse_string(acc, <<>>) do
+		{:error, "Unexpected end of buffer."}
+	end
+
+
+	defp parse_string(acc, <<char::utf8>>) when char == 34 do
+		result = acc |> Enum.reverse() |> List.to_string()
+		{:ok, result}
+	end
+
+	defp parse_string(acc, <<char::utf8, rest::binary>>) do
+		parse_string([char | acc], rest)
+	end
+
+	defp parse_string(acc, json) do 
+		{:error, "Could not decode char in string."}
+	end
 end
