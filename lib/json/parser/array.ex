4efd7908e28d22707274570a8626fdb241b4de23
Ivan Bozhilov <ivanbojilov@gmail.com>
2018-05-07 04:54:45 +0300

    Implementing JSON array parser

--- lib/json/parser/array.ex
+++ lib/json/parser/array.ex
@@ -1,5 +1,26 @@
 defmodule JSON.Parser.Array do
-	def parse(json) do
-		"array " <> json
+	def parse(<<"["::utf8, rest::binary>>) do
+		case parse_array([], "", rest) do
+			{:ok, result} -> result
+			{:error, reason} -> reason
+		end
 	end 
+
+	defp parse_array(acc, item_acc, <<93>>) do
+		acc = [item_acc | acc]
+		result = acc |> Enum.reverse() |> Enum.map(fn(x) -> JSON.Parser.parse(x) end)
+		{:ok, result}
+	end
+
+	defp parse_array(acc, item_acc, <<44, rest::binary>>) do
+		parse_array([item_acc | acc], "", rest)
+	end
+
+	defp parse_array(acc, item_acc, <<char::utf8, rest::binary>>) do
+		parse_array(acc, item_acc <> <<char>>, rest)
+	end
+
+	defp parse_array(acc, item_acc, <<>>) do
+		{:error, "Unexpected end of buffer"}
+	end
 end
