3f5889979f625975f6f2c96aab71f087e041c1b5
Ivan Bozhilov <ivanbojilov@gmail.com>
2018-05-08 14:00:17 +0300

    returning a tuple with status instead of result

--- lib/json/parser.ex
+++ lib/json/parser.ex
@@ -1,33 +1,45 @@
 defmodule JSON.Parser do
-	def parse("null") do
-		nil
-	end
+  def parse(<<json::binary>>) do
+    case parse_json(json) do
+      {:ok, result, <<>>} -> result
+      {:ok, result, rest} -> {:error, {:has_rest, result, rest}}
+      {:error, reason} -> {:error, reason}
+    end
+  end
 
-	def parse(<<"true"::utf8>>) do
-		true
-	end
+  def parse(data) do
+    {:error, "Non binary data provided" <> data}
+  end
 
-	def parse(<<"false"::utf8>>) do
-		false
-	end
+  defp parse_json(<<"null"::utf8, rest::binary>>) do
+    {:ok, nil, rest}
+  end
 
-	def parse(<<"{"::utf8, _::binary>> = object) do
-		JSON.Parser.Object.parse(object)
-	end
+  defp parse_json(<<"true"::utf8, rest::binary>>) do
+    {:ok, true, rest}
+  end
 
-	def parse(<<"["::utf8, _::binary>> = array) do
-		JSON.Parser.Array.parse(array)
-	end
+  defp parse_json(<<"false"::utf8, rest::binary>>) do
+    {:ok, false, rest}
+  end
 
-	def parse(<<digit::utf8, _::binary>> = number) when digit in 48..57 do
-		JSON.Parser.Number.parse(number)
-	end
+  defp parse_json(<<"{"::utf8, _::binary>> = object) do
+    JSON.Parser.Object.parse(object)
+  end
 
-	def parse(<<"-"::utf8, _::binary>> = number) do
-		JSON.Parser.Number.parse(number)
-	end
+  defp parse_json(<<"["::utf8, _::binary>> = array) do
+    JSON.Parser.Array.parse(array)
+  end
 
-	def parse(<<quatation::utf8, _::binary>> = string) when quatation == 34 do 
-		JSON.Parser.String.parse(string)
-	end
+  defp parse_json(<<digit::utf8, _::binary>> = number) when digit in 48..57 do
+    JSON.Parser.Number.parse(number)
+  end
+
+  defp parse_json(<<"-"::utf8, _::binary>> = number) do
+    JSON.Parser.Number.parse(number)
+  end
+
+  defp parse_json(<<quatation::utf8, _::binary>> = string) when quatation == 34 do
+    JSON.Parser.String.parse(string)
+  end
 end
