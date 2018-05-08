0ed37d5908ee943564c93da67d79067a329d8d44
Ivan Bozhilov <ivanbojilov@gmail.com>
2018-05-08 14:48:08 +0300

    Adding format function to omit whitespaces

--- lib/json/parser/array.ex
+++ lib/json/parser/array.ex
@@ -1,31 +1,47 @@
 defmodule JSON.Parser.Array do
   def parse(<<"["::utf8, rest::binary>>) do
-    rest |> String.trim() |> JSON.Parser.parse() |> process_array([])
+    rest 
+    |> String.trim() 
+    |> JSON.Parser.parse() 
+    |> format()
+    |> process_array([])
   end
 
   def parse(_) do
     {:error, "Unexpected data"}
   end
 
-  defp process_array({:error, {:has_rest, item, <<"]"::utf8, rest::binary>>}}, acc) do
+  defp format(result) when not is_tuple(result) do
+    {result, <<>>}
+  end
+
+  defp format({:error, {:has_rest, result, rest}}) do
+    {result, String.trim(rest)}
+  end
+
+  defp format({:error, reason}) do 
+    {:error, reason}
+  end
+
+  defp process_array({item, <<"]"::utf8, rest::binary>>}, acc) do
     acc = [item | acc]
     result = Enum.reverse(acc)
     {:ok, result, rest}
   end
 
-  defp process_array({:error, {:has_rest, item, <<","::utf8, rest::binary>>}}, acc) do
-    rest |> String.trim() |> JSON.Parser.parse() |> process_array([item | acc])
+  defp process_array({item, <<","::utf8, rest::binary>>}, acc) do
+    rest 
+    |> String.trim() 
+    |> JSON.Parser.parse() 
+    |> format()
+    |> process_array([item | acc])
   end
 
-  defp process_array({:error, {:has_rest, _, _}}) do
+  defp process_array({item, binary}, acc) do
     {:error, "Unexpected end of buffer"}
   end
 
   defp process_array({:error, reason}, acc) do
     {:error, reason}
-  end
-
-  defp process_array(result, acc) when not is_tuple(result) do
-    {:error, "Unexpected end of buffer"}
   end
 end
